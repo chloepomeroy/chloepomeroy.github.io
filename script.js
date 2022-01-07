9728853a5f38cc35ed11d5f41fb5ef080ecca19c
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2022-01-06 22:53:55 -0500

    commit

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 function compute()
 {
-    //Store data from the form into variables
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-
-    //Check that the principal amount is positive
     if(principal<=0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-
-    //if the amount is positive, return the interest amount
-    document.getElementById("result").innerHTML="If you deposit "+<mark>principal</mark>+",\<br\>at an interest rate of "+<mark>rate</mark>+"%\<br\>You will receive an amount of "+<mark>interest</mark>+",\<br\>in the year "+<mark>year</mark>+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
     return true;
 }
-
-
 function updateRate() 
 {
-    //Update the interest rate value as a user moves the slider
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
